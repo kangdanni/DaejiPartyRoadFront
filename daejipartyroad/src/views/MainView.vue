2470745850b1b585832f19c3ccbdb60ebc425ea6
강단 <kkangdanni@sk.com>
2021-12-05 21:58:50 +0900

    main화면 card섹션 생성

--- daejipartyroad/src/views/MainView.vue
+++ daejipartyroad/src/views/MainView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="main">
     <DaejiPartyRoad msg="여기는 돼지파티의 성지입니다." />
+    <MainStoreList></MainStoreList>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import DaejiPartyRoad from "@/components/DaejiPartyRoad.vue";
+import MainStoreList from "@/components/MainStoreList.vue";
 
 export default {
   name: "Main",
   components: {
     DaejiPartyRoad,
+    MainStoreList,
   },
 };
 </script>
