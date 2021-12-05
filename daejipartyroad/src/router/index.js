4d9e3e7437c4a96e0c88ad1ad40453645fee1a7d
강단 <kkangdanni@sk.com>
2021-12-05 19:09:18 +0900

    sample Component

--- daejipartyroad/src/router/index.js
+++ daejipartyroad/src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import MainView from "../views/MainView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
+    name: "main",
+    component: MainView,
+  },
+  {
+    path: "/home",
     name: "Home",
     component: Home,
   },
@@ -16,8 +22,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
 ];
 
---
+++ daejipartyroad/src/views/MainView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main">
+    <DaejiPartyRoad msg="여기는 돼지파티의 성지입니다." />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import DaejiPartyRoad from "@/components/DaejiPartyRoad.vue";
+
+export default {
+  name: "Main",
+  components: {
+    DaejiPartyRoad,
+  },
+};
+</script>
